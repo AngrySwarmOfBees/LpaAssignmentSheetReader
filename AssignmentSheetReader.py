eedfa3a1ab6f7e447fe5933cb4e8efeb04dcd34f
Bee <ben.mowery@logosprep.org>
2022-05-05 14:11:24 -0500

    Program can now read out entire assignment sheet

--- AssignmentSheetReader.py
+++ AssignmentSheetReader.py
@@ -1,6 +1,8 @@
 #this is a system that when supplied with a Logos prep assignment sheet, can read out assignment sheets and do multiple things with that data
 
 #setting up packages
+from asyncio.windows_events import NULL
+from types import NoneType
 import docx
 from docx import Document
 import sys
@@ -12,6 +14,9 @@ FileName = "" #String, Contains the name of the assignment sheet but not the fil
 FileType = "" #String, format will be in a standard file extension IE: "doc", will be grabbed when file is chosen by reading it from file name      !in use
 SupportedFileTypes = ["docx", "doc"] #These are the only two types of files that assignment sheets will be made as
 FileInput = "" #String, Contains both the file name and file extension      -now unnessecary
+NoFileErrors = True
+global DocumentRef
+DocumentRef = NoneType
 
 #collecting launch arg data
 LaunchArgument = str(sys.argv[1])
@@ -24,13 +29,41 @@ FileType = FileInfo[1]  #saving file extension
 print(FileType)     #output(remove in release)
 
 #Checking that file type is supported, and that file exists
-if DocPath.is_file() == False:      #this will be replaced with a popup (when this program is given a GUI)
-    print("Error: " + LaunchArgument + " could not be found, please verify that the path is correct")
-    exit()      #will not exit when program has GUI, will only allow to chose another path
-if SupportedFileTypes.__contains__(FileType) == False:      #this will be replaced with a popup (when this program is given a GUI)
-    print("Error: " + FileType + " files are not currently supported")
-    exit()      #will not exit when program has GUI, will only allow to chose another path
+def CheckForFile():
+    if DocPath.is_file() == False:      #this will be replaced with a popup (when this program is given a GUI)
+        print("Error: " + LaunchArgument + " could not be found, please verify that the path is correct")
+        NoFileErrors = False
+        exit()      #will not exit when program has GUI, will only allow to chose another path
+def CheckIfIsSupported():
+    if SupportedFileTypes.__contains__(FileType) == False:      #this will be replaced with a popup (when this program is given a GUI)
+        print("Error: " + FileType + " files are not currently supported")
+        NoFileErrors = False
+        exit()      #will not exit when program has GUI, will only allow to chose another path
 
+CheckForFile()
+CheckIfIsSupported()
+
+#Accessing file
+def OpenFile():
+    if NoFileErrors == True:        #will only run if there are no file errors
+        global DocumentRef
+        DocumentRef = docx.Document(DocPath)      #opens the doccument and creates a variable that refrences it
+    else:
+        print("Error: file error")      #seccond warning, though this function should not even be able to be run in the first place if there is a file error
+        exit()      #will not exit when program has GUI, will throw pop up
+
+OpenFile()
+
+#Reading out file info
+def GetDoccumentData():
+    global DocumentRef
+    for p in DocumentRef.tables:
+        for q in p.rows:
+            for r in q.cells:
+                for s in r.paragraphs:
+                    print(s.text)
+
+GetDoccumentData()
 
 
 
